7b799c6d30e108806aa257a7d3038df69562d857
Lauden Eller <laudlauden@gmail.com>
2022-05-17 13:25:58 -0700

    Tags still not added to posts but alert window is now working for deleting tags

--- src/components/tags/AllTags.js
+++ src/components/tags/AllTags.js
@@ -5,6 +5,7 @@ import { useHistory } from "react-router-dom";
 
 export const AllTags = () => {
     const [tags, setTags] = useState([])
+    const [showAlert, setShowAlert] = useState(0)
 
     const getTags = () => {
         getAllTags()
@@ -20,12 +21,39 @@ export const AllTags = () => {
 
     const history = useHistory()
 
-    const DeleteTag = (id) => {
-        deleteTag(id).then(getTags)
+    const notifyOnClickDelete = () => {
+        return(
+            <>
+                <div className="modal">
+                    <div className="modal-content">
+                        <div className="alert-text">
+                            {
+                                showAlert != -1 ? <p>Are you sure you wish to delete this post?</p>
+                                :
+                                <p>Post Successfully Deleted.</p>
+                            }
+                        </div>
+                        <div className="alert-buttons">
+                            {
+                                showAlert != -1 ? <><button onClick={()=>{
+                                    deleteTag(showAlert).then(()=>{setShowAlert(-1)
+                                    getTags()})}}>Yes</button>
+                                <button onClick={()=>setShowAlert(0)}>No</button></>
+                                :
+                                <button onClick={()=>setShowAlert(0)}>Close</button>
+                            }
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
     }
 
 return (
     <>
+    {
+        showAlert != 0 ? notifyOnClickDelete() : ""
+    }
         <div className="CreateNewTagFormContainer">
             <NewTagForm getTags={getTags} />
         </div>
@@ -33,7 +61,7 @@ return (
             return <div key={`tag--${tag.id}`}>{tag.label} 
             {localStorage.getItem("staff") === "true" ? <>
             <button onClick={() => history.push(`./tags/${tag.id}`)}>edit</button> 
-            <button onClick={() => {DeleteTag(tag.id)}}>delete</button>
+            <button onClick={() => {setShowAlert(tag.id)}}>delete</button>
             </> : null }
             </div>
         })}
