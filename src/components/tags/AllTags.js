27866ee486d68311234a5b9927f775017b28767c
Lauden Eller <laudlauden@gmail.com>
2022-05-17 13:06:26 -0700

    Tags have not been added to posts, but the conditional rendering of rag buttons works

--- src/components/tags/AllTags.js
+++ src/components/tags/AllTags.js
@@ -3,10 +3,7 @@ import React, { useEffect, useState } from "react";
 import { NewTagForm } from "./CreateTagForm";
 import { useHistory } from "react-router-dom";
 
-// If tags have custom property, render edit and delete buttons
-
 export const AllTags = () => {
-
     const [tags, setTags] = useState([])
 
     const getTags = () => {
@@ -15,7 +12,6 @@ export const AllTags = () => {
                     setTags(tags)
                 }))
     }
-
 
     useEffect(() => {
         getTags()
@@ -29,14 +25,13 @@ export const AllTags = () => {
     }
 
 return (
-    <> 
-    <div>AllTags Page</div>
+    <>
         <div className="CreateNewTagFormContainer">
             <NewTagForm getTags={getTags} />
         </div>
         {tags.map((tag) => { 
             return <div key={`tag--${tag.id}`}>{tag.label} 
-            {localStorage.getItem("staff") === true ? <>
+            {localStorage.getItem("staff") === "true" ? <>
             <button onClick={() => history.push(`./tags/${tag.id}`)}>edit</button> 
             <button onClick={() => {DeleteTag(tag.id)}}>delete</button>
             </> : null }
